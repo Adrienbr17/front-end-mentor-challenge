98a3e771e0812d0fc133c6a314da36695ee57d98
adrien.br17 <a.brissaud@enssop.fr>
2022-01-17 14:59:08 +0100

    Sunrise Agency Projet front end mentor

---
+++ script.js
@@ -0,0 +1,6 @@
+let navPhone = document.getElementById('navigation-telephone');
+let hamburger = document.getElementById('hamburger');
+
+hamburger.addEventListener('click', function(){
+    navPhone.classList.toggle("active");
+})
